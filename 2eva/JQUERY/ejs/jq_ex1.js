09a3574a09645c6f6d27876708db839208c070fa
solvam <alumno@solvam.local>
2025-12-03 12:17:13 +0100

    31225

---
+++ 2eva/JQUERY/ejs/jq_ex1.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    console.log($("p"));
+    console.log($("#todo-list"));
+    console.log($("p, li.b"));
+    console.log($("*"));
+    console.log($("#important.a"));
+    console.log($("ul+p"));
+    console.log($("p"));
+    console.log($("p"));
+    console.log($("p"));
+});
+
+
